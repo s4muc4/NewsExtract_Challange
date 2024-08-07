f83d9ba9bc655fd5dce1ad3fc43eb3784fcc76f4
s4muc4 <eng.comp.samuel@hotmail.com>
2024-08-07 19:08:17 -0300

    feature: get all informations of each new with specific count of news

--- src/Latimes.py
+++ src/Latimes.py
@@ -8,20 +8,23 @@ import re
 from typing import List
 
 class LatimesExtractor:
-    def __init__(self) -> None:
+    def __init__(self,count_news: int, phrase, sort_by, date) -> None:
         """Initializing class of website"""
         self.browser = Selenium()
         self.table = Tables()
-
+        self.count_news = count_news
+        self.date = date
+        self.phrase = phrase
+        self.sort_by = sort_by
     def open_specific_browser(self):
         self.browser.open_browser(url="https://www.latimes.com/", browser="firefox")
 
     def close_browser(self):
         self.browser.close_browser()
 
-    def search_by_phrase(self, phrase):
+    def search_by_phrase(self):
         self.browser.click_button("//button[@data-element='search-button']")
-        self.browser.input_text("//input[@data-element='search-form-input']", phrase)
+        self.browser.input_text("//input[@data-element='search-form-input']", self.phrase)
         self.browser.press_keys(None, "RETURN")
         try:
             self.browser.wait_until_element_is_enabled("//span[@class='search-results-module-count-desktop']","15")
@@ -29,32 +32,54 @@ class LatimesExtractor:
             regex_numbers = re.findall(r'\d+', result)
             news_count = ''.join(regex_numbers)
             self.browser.wait_until_element_is_enabled("//select[@class='select-input']","30")
-            self.browser.select_from_list_by_value("//select[@class='select-input']","1")
+            try:
+                self.browser.select_from_list_by_label("//select[@class='select-input']",self.sort_by)
+            except Exception as err:
+                self.browser.select_from_list_by_label("//select[@class='select-input']","Relevance")
+                
         except Exception as err:
             self.browser.wait_until_element_is_enabled("//div[@class='search-results-module-no-results']","30")
             news_count = self.browser.get_text("//div[@class='search-results-module-no-results']")
         finally:
-            print(news_count)
+            print(str(news_count) +" results found." )
         return int(news_count)
     
     def get_page_news(self):
-        news:List[WebElement] = self.browser.get_webelements("//ul[@class='search-results-module-results-menu']/li/ps-promo") 
-        
-        for new in news:
-            # Extract promo title
-            title = new.find_element(By.CLASS_NAME, "promo-title").text
-            date = new.find_element(By.CLASS_NAME, "promo-timestamp").text
-            description = new.find_element(By.CLASS_NAME, "promo-description").text
-            picture_link = new.find_element(By.CLASS_NAME, "image").get_attribute("srcset")
-            picture_file_name = self.get_image_file_name(picture_link)
-            href = new.find_element(By.TAG_NAME, "a").get_attribute("href")
-            print("-----------------------------------------------------------------------------------------------------------------------")
-            print("Title: "+title)
-            print("Post Date: "+date)
-            print("Description: "+description)
-            print("Picture file name: "+picture_file_name)
-            print("URL of new: "+href)
-            print("-----------------------------------------------------------------------------------------------------------------------")
+        count_news_found = 0
+        finished = False
+        while finished == False or count_news_found < self.count_news:
+            news:List[WebElement] = self.browser.get_webelements("//ul[@class='search-results-module-results-menu']/li/ps-promo") 
+            for new in news:
+                if count_news_found < self.count_news: 
+                    title = new.find_element(By.CLASS_NAME, "promo-title").text
+                    try:
+                        topic = new.find_element(By.XPATH, "//p[@class='promo-category']/a").text
+                        date = new.find_element(By.CLASS_NAME, "promo-timestamp").text
+                        description = new.find_element(By.CLASS_NAME, "promo-description").text
+                        picture_link = new.find_element(By.CLASS_NAME, "image").get_attribute("srcset")
+                        picture_file_name = self.get_image_file_name(picture_link)
+                        href = new.find_element(By.TAG_NAME, "a").get_attribute("href")
+                        count_news_found += 1
+                        print("-----------------------------------------------------------------------------------------------------------------------")
+                        print("New number: " + str(count_news_found))
+                        print("Title: "+title)
+                        print("Topic: "+topic)
+                        print("Post Date: "+date)
+                        print("Description: "+description)
+                        print("Picture file name: "+picture_file_name)
+                        print("URL of new: "+href)
+                        print("-----------------------------------------------------------------------------------------------------------------------")
+                        """Send data to excel file"""
+                    except Exception as err:
+                        print("Error to get new from " + title)
+                else:
+                    finished = True
+            if finished == False:
+                try:
+                    self.browser.click_element_when_clickable("//div[@class='search-results-module-next-page']",10)
+                except TimeoutError:
+                    finished = True
+
 
     def get_image_file_name(self, srcset):
         evidences = srcset.split('%')
@@ -71,7 +96,9 @@ class LatimesExtractor:
             filename = "Filename not found because is without extension"
         return filename
         
+
         
+    
 
     
     
--- tasks.py
+++ tasks.py
@@ -7,14 +7,14 @@ def news_extract():
 
     for item in workitems.inputs:
         phrase = item.payload["phrase"]
-        topic = item.payload["topic"]
+        sort_by = item.payload["sort_by"]
         history = item.payload["history"]
         print("#######################################################################################################################")
-        print("Searching phrase ("+phrase+") in topic ("+topic+") since ("+str(history)+") months")
+        print("Searching phrase ("+phrase+") in order by ("+sort_by+") since ("+str(history)+") months")
         print("#######################################################################################################################")
-        extractor = LatimesExtractor()
+        extractor = LatimesExtractor(count_news=15, phrase=phrase, sort_by=sort_by, date=history)
         extractor.open_specific_browser()
-        news_count = extractor.search_by_phrase(phrase)
+        news_count = extractor.search_by_phrase()
         if news_count > 0:
             extractor.get_page_news()
         extractor.close_browser()
